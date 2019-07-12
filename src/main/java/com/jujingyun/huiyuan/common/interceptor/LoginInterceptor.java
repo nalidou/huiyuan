422b668f4b5fcc95891f5987633d2084120450f3
wuzhy <wuzhy@rd.netease.com>
2019-07-12 11:25:35 +0800

    MOD:uri拦截器

--- src/main/java/com/jujingyun/huiyuan/common/interceptor/LoginInterceptor.java
+++ src/main/java/com/jujingyun/huiyuan/common/interceptor/LoginInterceptor.java
@@ -2,7 +2,11 @@ package com.jujingyun.huiyuan.common.interceptor;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jujingyun.huiyuan.common.entity.User;
+import com.jujingyun.huiyuan.controller.UserController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +15,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class LoginInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginInterceptor.class);
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
@@ -26,12 +32,15 @@ public class LoginInterceptor implements HandlerInterceptor {
                 out.print(json.toJSONString());
                 out.flush();
                 out.close();
-
-            } catch (IOException e) {
-
-            }
+            } catch (IOException e) {}
             return false;
         }
         return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        log.info("REQUSET URI --- " + request.getRequestURI());
     }
 }
